bd28a660aeb26e1a3cdad00d1bc2209148daf8d3
Antoine Galluet <antoine18@gmail.com>
2014-04-17 11:02:14 +0200

    Configuration tests

--- Android_App/app/src/main/java/com/example/lo53_indoor_positionning_system/app/MainActivity.java
+++ Android_App/app/src/main/java/com/example/lo53_indoor_positionning_system/app/MainActivity.java
@@ -1,23 +1,30 @@
-package com.example.lo53_positioning_system.app;
+package com.example.lo53_indoor_positionning_system.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView coucou = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coucou = new TextView(this);
+        coucou.setText("Bonjour, vous me devez 1 000 000€.");
+        setContentView(coucou);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
